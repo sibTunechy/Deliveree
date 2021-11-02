55aa658bed7c67fe9108540f239763343a3e6c14
sibTunechy <86810831+sibTunechy@users.noreply.github.com>
2021-11-02 13:39:43 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+/*======== Show Navbar ==========*/
+const navbar = document.querySelector(".navbar");
+const hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", () =>{
+    navbar.classList.toggle("show");
+});
+
+/*==========Scroll Position ==========*/
+
+
+/*=========== Smooth Scroll ======*/
+
+
+/*=========== Preloader ==========*/
+
+
+/*==========Scroll Top ============*/
+
+
+/*========= Scroll Reveal ==========*/
